89cad077b54945177100b563608b129260dfb7f3
Léo Sarochar <leo.sarochar@epitech.eu>
2021-05-05 23:33:29 +0200

    Cleaning

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 all:
-	gcc ex01/*.c -o ex01/ex01
-	gcc ex02/*.c -o ex02/ex02
-	gcc ex03/*.c -o ex03/ex03
-	gcc ex04/*.c -o ex04/ex04
-	gcc ex05/*.c -o ex05/ex05
-	gcc ex06/*.c -o ex06/ex06
+	gcc -g3 ex01/*.c -o ex01/ex01
+	gcc -g3 ex02/*.c -o ex02/ex02
+	gcc -g3 ex03/*.c -o ex03/ex03
+	gcc -g3 ex04/*.c -o ex04/ex04
+	gcc -g3 ex05/*.c -o ex05/ex05
+	gcc -g3 ex06/*.c -o ex06/ex06
 
 fclean:
 	rm -f ex01/ex01
--- ex05/main.c
+++ ex05/main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-    char *s = "Goal";
-    char *t = "Home";
-    while (*s++ = *t++)
-        printf(*s);
+int main() {
+    int *ptr = malloc(sizeof(int));
+    *ptr = 9;
+    printf("I love the number %d\n", *ptr);
+
+    free(ptr);
+    *ptr = 20;
+
+    int *arr = malloc(5 * sizeof(int));
+    for (int i = 0; i <= 5; i++) {
+        arr[i] = i;
+    }
+    free(arr);
     return 0;
 }
